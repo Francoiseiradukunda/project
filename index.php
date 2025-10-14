2bf5c6562133ae2f327d1d10cef1a32062d1fd4d
iradufrance <francoise1.iradukunda@gmail.com>
2025-10-14 15:07:47 +0300

    firsr

--- index.php
+++ index.php
@@ -3,6 +3,22 @@
         <title></title>
     </head>
     <body>
-        <h1>WELLCOME ON MY WEBSITE</h1>
+    
+        <div class="navcontainer">
+            <div class="van2">
+<nav class="navigation">
+    <ul class="list1">
+        <li><a href="">home</a></li>
+        <li><a href="">about-us</a></li>
+        <li><a href="">contact-us</a></li>
+        <li><a href="">stakeholders</a></li>
+        <li><a href="">support</a></li>
+        <li><a href="">logout</a></li>
+    </ul>
+
+</nav>
+</div>
+        </div>
+            <h1>WELLCOME ON MY WEBSITE</h1>
     </body>
 </html>
